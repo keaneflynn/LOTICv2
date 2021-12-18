1fafe8fcd01e947d369d449b112137e6b5c5061c
Keane Flynn <42682624+keaneflynn@users.noreply.github.com>
2021-12-18 15:24:52 -0800

    Update realsense.py

--- realsense.py
+++ realsense.py
@@ -18,7 +18,6 @@ class RealSense:
         depth_frame = np.asanyarray(depth_frame.get_data())
         color_frame = np.asanyarray(color_frame.get_data())
         if depth_frame is None or color_frame is None:
-        #if not depth_frame or not color_frame:
             print("No frames detected, check camera connection")
             exit(1)
         return True, depth_frame, color_frame
