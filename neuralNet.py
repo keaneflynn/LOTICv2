0287da49bf914f85eeafa8360d6c9e4bda203ff5
keaneflynn <kmflynn24@berkeley.edu>
2021-12-18 20:04:22 -0800

    test

--- neuralNet.py
+++ neuralNet.py
@@ -24,5 +24,4 @@ class neuralNet:
         #net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU) #default processing target
 
         model = cv2.dnn_DetectionModel(net)
-        #model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
-        model.setInputParams(size=(416, 416), scale=1/float(255.0), swapRB=True) #float is important for Python version 2!!!
+        model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
