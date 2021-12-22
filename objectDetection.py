004a71abee8f7ed49b6a397b82341f43c0048ad0
keaneflynn <kmflynn24@berkeley.edu>
2021-12-21 19:53:27 -0800

    untested functional object detection beginning loop

--- objectDetection.py
+++ objectDetection.py
@@ -1,25 +1,26 @@
 import cv2
 
 class objectDetection:
-    def __init__(self):
-        self.confidence = 0.5
-        self.nmsThreshold = 0.1
-        self.colors = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+    def __init__(self, confidence_activation, weights_file, config_file, names_file):
+        self.confidence = confidence_activation
+        self.nmsThreshold = 0.2
+        self.color = (0, 255, 255)
+
+        self.weights_file = weights_file
+        self.config_file = config_file
 
         class_names = []
-        with open("models/coco.names", "r") as f:
+        with open(names_file, "r") as f:
             class_names = [cname.strip() for cname in f.readlines()]
 
     def loadNN(self):
-        #vc = cv2.VideoCapture('rtsp://admin:admin123456@192.168.1.12:554') #for use with IP camera
-
-        net = cv2.dnn.readNet("models/yolov4-tiny.weights", "models/yolov4-tiny.cfg")
+        net = cv2.dnn.readNet(self.weights_file, self.config_file)
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA) #must be enabled for GPU use
         net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16) #reduced latency nn processing, slight hit on accuracy
-        #net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA) #higher accuracy target use
+        #net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA) #higher accuracy target use, slower
 
-        model = cv2.dnn_DetectionModel(net)
-        model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+        self.model = cv2.dnn_DetectionModel(net)
+        self.model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
-    def detection(self):
-        classes, scores, boxes = model.detect(frame, self.confidence, self.nmsThreshold)
+    def detection(self, frame):
+        classes, scores, boxes = self.model.detect(frame, self.confidence, self.nmsThreshold)
