849cfcf9c07a0c6e3f06918120b20e821472d4d7
keaneflynn <kmflynn24@berkeley.edu>
2021-12-23 14:42:38 -0800

    framework created for tracker

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+import multiprocessing as mp
+
 from argparse import ArgumentParser
 from objectDetection import objectDetection
 from frameImport import frameImport
@@ -6,30 +8,36 @@ from frameImport import frameImport
 
 def main():
     parser = ArgumentParser(description='LOTICv2')
-    parser.add_argument('VideoSource', type=str, help='identify your video source, using "realsense" will result in use with realsense camera')
-    parser.add_argument('WeightsFile', type=str, help='YOLO weight file')
-    parser.add_argument('ConfigFile', type=str, help='YOLO configuration file')
-    parser.add_argument('NamesFile', type=str, help='YOLO class names file')
-    parser.add_argument('SiteCode', type=str, help='Site location ex) WillowCreek')
-    parser.add_argument('StreamSide', type=str, default='RR', help='river right (RR) or river left (RL)')
-    parser.add_argument('ConfigFile', type=str, help='YOLO configuration file')
-    parser.add_argument('ConfidenceActivation', type=float, default=0.2, help='confidence threshold to activate main loop')
-    parser.add_argument('ExitThreshold', type=int, help='Seconds after leaving screen to disable main loop')
-    parser.add_argument('MediaOutput', type=str, default='image', help='image or video output (note video output will bog down throughput')
+    parser.add_argument('video_source', type=str, help='identify your video source, using "realsense" will result in use with realsense camera')
+    parser.add_argument('weights_file', type=str, help='YOLO weight file')
+    parser.add_argument('config_file', type=str, help='YOLO configuration file')
+    parser.add_argument('names_file', type=str, help='YOLO class names file')
+    parser.add_argument('site_code', type=str, help='Site location ex) WillowCreek')
+    parser.add_argument('stream_side', type=str, default='RR', help='river right (RR) or river left (RL)')
+    parser.add_argument('confidence_activation', type=float, default=0.2, help='confidence threshold to activate main loop')
+    parser.add_argument('exit_threshold', type=int, help='Seconds after leaving screen to disable main loop')
+    parser.add_argument('media_output', type=str, default='image', help='image or video output (note video output will bog down throughput')
     args = parser.parse_args()
     
-    od = objectDetection(args.ConfidenceActivation, args.WeightFile, args.ConfigFile, args.NamesFile) #initialize class list and model params
-    fi = frameImport(args.VideoSource) #takes in args flag for video file and chooses between intel realsense camera
+    od = objectDetection(args.confidence_activation,
+                         args.weights_file,
+                         args.config_file,
+                         args.names_file) #initialize class list and model params
+    fi = frameImport(args.video_source) #takes in args flag for video file and chooses between intel realsense camera
     color_frame = fi.loadFrame()
     od.loadNN()
 
     while cv2.waitKey(1) < 1:
-        grabbed, frame = color_frame.read()
+        grabbed, frame = color_frame.read() #add multithreading
         if not grabbed:
             break
-        classes, scores, boxes = od.detection(frame)
-        objectTracker()
-        etc . . .
+        classes, scores, boxes = od.detection(frame) 
+        ot = objectTracker(frame, 
+                      classes,
+                      scores,
+                      boxes,
+                      args.stream_side,
+                      args.exit_threshold)
 
 if __name__ == '__main__':
     main()
---
+++ objectTracker.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as mp
+import math #needed for math.hypot (needed for comparing objects between frames)
+
+
+
+class objectTracker:
+    def __init__(self, frame, classes, scores, boxes, stream_side, exit_threshold):
+        self.frame = frame
+        self.classes = classes
+        self.scores = scores
+        self.boxes = boxes
+        self.stream_side = stream_side
+        self.exit_threshold = exit_threshold
+
+    def tracker(self):
+
+    def classLock(self):
+
+    def maxConf(self):
+
+    def travelDirection(self):
+
+    def objectLength(self): #Keane project for realsense shit
