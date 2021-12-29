8174422653400a6b11ea0165da124c23f63c529f
keaneflynn <kmflynn24@berkeley.edu>
2021-12-29 11:59:43 -0800

    json push from new computer

--- jsonOut.py
+++ jsonOut.py
@@ -1,7 +1,39 @@
 import json
+import numpy as np
 
 class jsonOut:
-	def __init__(self):
-		stuff
-	def writeFile (self):
+	def __init__(self, sitename, INPUTS_FROM_TRACKER_HERE):
+		self.dateTime =
+		self.site = sitename
+		self.species = 
+		self.confidence =
+		self.travelDirection = 
+		self.fishLength =
 
+	def writeFile (self):
+		directory = 'outfile'
+		filename = 
+		json_file = {
+			"dateTime":,
+			"site":,
+			"species":,
+			"maxConfidence": np.float64(),
+			"travelDirection":
+		}
+
+		with open("{}/{}.json".format(directory, filename), 'w') as f:
+                json.dump(json_out, f)
+
+	def writeFile_rs (self):
+		directory = 'outfile'
+		filename = 
+		json_file = {
+			"dateTime":,
+			"site":,
+			"species":,
+			"maxConfidence": np.float64(),
+			"travelDirection":,
+			"fishLength_mm": np.float64()
+		}
+
+		with open("{}/{}.json".format(directory, filename), 'w') as f:
