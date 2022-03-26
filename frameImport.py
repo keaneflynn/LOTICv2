58bacfe26dff69470d5af4f04ba663fd4ac3f31a
keaneflynn <kmflynn24@berkeley.edu>
2022-03-26 16:43:29 -0700

    multithreading implemented and functional, need to go back and add exceptions to merge threads when process is killed

--- frameImport.py
+++ frameImport.py
@@ -1,9 +1,19 @@
 import cv2
+import queue
 
 class frameImport:
     def __init__(self, video_input):
         self.video_input = video_input
+        self.frame_input_queue = queue.Queue(100)
 
-    def loadFrame(self):
-        color_frame = cv2.VideoCapture(self.video_input)
-        return color_frame
+    def videoSource(self):
+        self.video_source = cv2.VideoCapture(self.video_input)
+        return self.video_source
+
+    def receiveFrame(self):
+        while True:
+            grabbed, self.frame = self.video_source.read()
+            self.frame_input_queue.put(self.frame)
+
+    def grabFrame(self):
+        return self.frame_input_queue.get()
