166d51b87c83912ce2f97671c34c48e81ff09306
keaneflynn <kmflynn24@berkeley.edu>
2022-12-05 15:32:53 -0800

    added automatic download script for USB drives

---
+++ bashScripts/usb_download.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SOURCE_FOLDER='/home/garcia/LOTIC/LOTICv2/outfile' #UPDATE WITH PROPER DIRECTIORY
+DESTINATION_FOLDER='/media/garcia/T7' #UPDATE WITH PROPER DIRECTIORY
+SITENAME='site1' #UPDATE WITH SITENAME FOR EACH UNIQUE COMPUTER
+FILENAME=$(date '+%m-%d-%y_%H-%M-%S')_$SITENAME
+FLASH_CHECK=$(lsusb | grep 'Samsung' | wc -l)
+DIRECTORY_CHECK=$(ls $SOURCE_FOLDER | wc -l)
+
+if [[ $FLASH_CHECK -gt 0 ]]
+then
+	if [[ $DIRECTORY_CHECK -lt 5 ]] #THRESHOLD FOR FILE TRANSFER IS 5 FILES CURRENTLY
+	then
+		echo 'No files to transfer'
+		udisksctl unmount -b /dev/sda1
+		udisksctl power-off -b /dev/sda1
+		exit 0
+	else
+	mv $SOURCE_FOLDER/* $DESTINATION_FOLDER
+	systemctl status usb_temp.service >> $DESTINATION_FOLDER/systemStatus.log
+	fi
+else
+	echo 'No USB drive detected'
+	exit 0
+fi
+
+cd $DESTINATION_FOLDER
+zip -q $FILENAME *.json *.avi *.log
+rm *.avi && rm *.json && rm *.log 
+
+cd ~/ 
+udisksctl unmount -b /dev/sda1 
+udisksctl power-off -b /dev/sda1 
+exit 0
+
